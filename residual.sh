bc855d45c5aa2a45b2d1660c82722e6e5679b82f
Dries Allaerts <D.J.N.Allaerts-1@tudelft.nl>
2022-10-05 10:58:55 +0200

    Save residual plot

--- residual.sh
+++ residual.sh
@@ -1,4 +1,10 @@
-#To run, execute `gnuplot residual.sh`
+#To run, execute `gnuplot residual.sh -`
+# Preparation to save plot to file
+set terminal push               # Store standard terminal for later use
+set terminal png size 400,300   # Set terminal to be a png file
+set output 'residual.png'       # Name png file
+
+# Read logfile and plot residuals
 set logscale y
 set title "Residuals"
 set ylabel 'Residual'
@@ -9,5 +15,12 @@ plot "< cat log.simpleFoam | grep 'Solving for Ux' | cut -d' ' -f9 | tr -d ','" 
 "< cat log.simpleFoam | grep 'Solving for omega' | cut -d' ' -f9 | tr -d ','" title 'omega' with lines,\
 "< cat log.simpleFoam | grep 'Solving for k' | cut -d' ' -f9 | tr -d ','" title 'k' with lines,\
 "< cat log.simpleFoam | grep 'Solving for p' | cut -d' ' -f9 | tr -d ','" title 'p' with lines
+
+# Plot to screen
+set terminal pop                # Retrieve standard terminal
+set output
+replot                          # Replot figure
+
+# Pause and repeat entire process
 pause 1
 reread
